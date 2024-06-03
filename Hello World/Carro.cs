3c6e58bdbc6fde4577b23a37b30d422037604357
viniciusCharleauxSoitic <vinicius.charleaux@soitic.com>
2024-06-03 18:11:25 -0300

    inciiando

---
+++ Hello World/Carro.cs
@@ -0,0 +1,13 @@
+namespace Hello_World
+{
+    class Carro
+    {
+        public void Ligar() {
+            Console.WriteLine("Ligando o carro");
+        }
+
+        public void Desligar() {
+            Console.WriteLine("Carro desligado");
+        }
+    }
+}
---
+++ Hello World/Program.cs
@@ -0,0 +1,14 @@
+﻿using Hello_World;
+
+namespace HelloWorld{
+    class Program {
+        static void Main(string[] args) {
+            Carro meuCarro = new Carro();
+
+            meuCarro.Ligar();
+            meuCarro.Desligar();
+
+        } 
+    }
+}
+
